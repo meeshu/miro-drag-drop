30ae5a850708da99f8be45fb361f38737b151709
meesh <meeshuagnihotri@gmail.com>
2023-07-14 15:44:55 -0700

    more figma images

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,20 @@ const { board } = miro;
 
 function App() {
   const images = [
-    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689369738/ui-images/age.svg"
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372606/ui-images/Status_kgn1aj.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372606/ui-images/sex_a6alzz.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372606/ui-images/Occupation_euhpcs.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372606/ui-images/Race_ymykly.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372605/ui-images/Income_Level_drrxwl.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372605/ui-images/Location_c8gqef.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372605/ui-images/gender_wwxxen.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372605/ui-images/Education_Level_l38z3m.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372605/ui-images/Ethnicity_vnuuwk.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689372604/ui-images/dob_kfjazi.svg",
+    "https://res.cloudinary.com/dp6w2kgzd/image/upload/v1689369738/ui-images/age.svg",
+
+
+
   ];
 
   const drop = async (e: DropEvent) => {
